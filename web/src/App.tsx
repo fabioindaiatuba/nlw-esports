63f7f63e047738c297f272e66d55efacca00e19b
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2022-09-12 22:38:12 -0300

    Inicio aula 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,13 @@
+import "./styles/main.css";
+import logoImg from "./assets/logo-nlw-esports.svg";
+
 function App() {
-  return <h1>Hello nlw</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" className="max-h-10" />
+      <h1>Seu duo está aqui.</h1>
+    </div>
+  );
 }
 
 export default App;
