29b608748c34d45f1648fc20725fab666f2e7080
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-09-14 17:22:12 -0300

    Aula 3 - Inicio backend server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,21 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
-  console.log("Acessou ads!");
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 4, name: "Anuncio 4" },
-  ]);
+app.get("/games", (request, response) => {
+  response.json({});
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const { id } = request.params;
+  return response.json({ id });
+});
+
+app.post("/ads", (request, response) => {
+  response.status(201).json({});
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  return response.json([]);
 });
 
 app.listen(3333);
