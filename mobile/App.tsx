1edfa8f34e144d9872beaf2d493211d949b659a6
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-09-14 12:12:23 -0300

    Aula 2 - finalizada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,24 @@
 import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
 import { Background } from "./src/components/Background";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
     <Background>
       <StatusBar
@@ -9,6 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
